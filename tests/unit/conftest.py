6ff20955f80b8d8332e5e50164c387683e240fa9
Alexander Berger <alexander.berger@jax.org>
2023-07-24 13:47:45 -0400

    Adding pyscopg error fixtures

---
+++ tests/unit/conftest.py
@@ -0,0 +1,18 @@
+"""Fixtures for all db unit tests."""
+import pytest
+from tests.unit.const import (
+    PSYCOPG_BASE_ERRORS,
+    PSYCOPG_ALL_ERRORS,
+)
+
+
+@pytest.fixture(params=PSYCOPG_ALL_ERRORS)
+def all_psycopg_errors(request):
+    """Return all psycopg errors."""
+    return request.param
+
+
+@pytest.fixture(params=PSYCOPG_BASE_ERRORS)
+def base_psycopg_errors(request):
+    """Return base psycopg errors."""
+    return request.param
