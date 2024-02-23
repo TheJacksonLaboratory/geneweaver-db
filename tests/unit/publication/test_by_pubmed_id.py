d1b280c0f41539adb533f82ce44924c119c2556a
Alexander Berger <alexander.berger@jax.org>
2024-02-23 11:30:26 -0500

    Adding code for async publication tests

--- tests/unit/publication/test_by_pubmed_id.py
+++ tests/unit/publication/test_by_pubmed_id.py
@@ -1,5 +1,6 @@
 """Test the get_publication_by_pubmed_id function."""
 import pytest
+from geneweaver.db.aio.publication import by_pubmed_id as async_by_pubmed_id
 from geneweaver.db.publication import by_pubmed_id
 
 from tests.unit.publication.const import (
@@ -7,9 +8,10 @@ from tests.unit.publication.const import (
     PUBMED_PUBLICATIONS,
 )
 from tests.unit.testing_utils import (
+    async_create_execute_raises_error_test,
+    async_create_fetchone_raises_error_test,
     create_execute_raises_error_test,
     create_fetchone_raises_error_test,
-    get_magic_mock_cursor,
 )
 
 test_by_pubmed_id_execute_raises_error = create_execute_raises_error_test(
@@ -20,16 +22,38 @@ test_by_pubmed_id_fetchone_raises_error = create_fetchone_raises_error_test(
     by_pubmed_id, "12345678"
 )
 
+test_async_by_pubmed_id_execute_raises_error = async_create_execute_raises_error_test(
+    async_by_pubmed_id, "12345678"
+)
+
+test_async_by_pubmed_id_fetchone_raises_error = async_create_fetchone_raises_error_test(
+    async_by_pubmed_id, "12345678"
+)
+
 
 @pytest.mark.parametrize(
     ("pmid", "expected_result"), PUBMED_PUBLICATIONS + EDGE_CASE_PUBMED_PUBLICATIONS
 )
-def test_get_publication_by_pubmed_id(pmid, expected_result):
+def test_get_publication_by_pubmed_id(pmid, expected_result, cursor):
     """Test getting a publication by pubmed id."""
     # Prepare the mock cursor
-    cursor = get_magic_mock_cursor(expected_result)
+    cursor.fetchone.return_value = expected_result
 
     result = by_pubmed_id(cursor, pmid)
 
     assert result == expected_result
     assert pmid in cursor.execute.call_args[0][1].values()
+
+
+@pytest.mark.parametrize(
+    ("pmid", "expected_result"), PUBMED_PUBLICATIONS + EDGE_CASE_PUBMED_PUBLICATIONS
+)
+async def test_async_get_publication_by_pubmed_id(pmid, expected_result, async_cursor):
+    """Test getting a publication by pubmed id."""
+    # Prepare the mock cursor
+    async_cursor.fetchone.return_value = expected_result
+
+    result = await async_by_pubmed_id(async_cursor, pmid)
+
+    assert result == expected_result
+    assert pmid in async_cursor.execute.call_args[0][1].values()
--- tests/unit/publication/test_by_pubmed_ids.py
+++ tests/unit/publication/test_by_pubmed_ids.py
@@ -2,6 +2,7 @@
 import random
 
 import pytest
+from geneweaver.db.aio.publication import by_pubmed_ids as async_by_pubmed_ids
 from geneweaver.db.publication import by_pubmed_ids
 
 from tests.unit.publication.const import (
@@ -9,9 +10,10 @@ from tests.unit.publication.const import (
     PUBMED_PUBLICATIONS,
 )
 from tests.unit.testing_utils import (
+    async_create_execute_raises_error_test,
+    async_create_fetchall_raises_error_test,
     create_execute_raises_error_test,
     create_fetchall_raises_error_test,
-    get_magic_mock_cursor,
 )
 
 test_by_pubmed_id_execute_raises_error = create_execute_raises_error_test(
@@ -22,6 +24,13 @@ test_by_pubmed_id_fetchone_raises_error = create_fetchall_raises_error_test(
     by_pubmed_ids, ["12345678", "23456"]
 )
 
+test_async_by_pubmed_id_execute_raises_error = async_create_execute_raises_error_test(
+    async_by_pubmed_ids, ["12345678", "23456"]
+)
+
+test_async_by_pubmed_id_fetchone_raises_error = async_create_fetchall_raises_error_test(
+    async_by_pubmed_ids, ["12345678", "23456"]
+)
 
 # We want repeatable tests, so let's set the random seed.
 random.seed(1)
@@ -37,13 +46,28 @@ params = [[l for l in zip(*subset)] for subset in subsets]  # noqa: B905, E741
 
 
 @pytest.mark.parametrize(("pmids", "expected_result"), params)
-def test_get_publications_by_pubmed_ids(pmids, expected_result):
+def test_get_publications_by_pubmed_ids(pmids, expected_result, cursor):
     """Test getting multiple publications by pubmed ids."""
     # Prepare the mock cursor
-    cursor = get_magic_mock_cursor(expected_result)
+    cursor.fetchall.return_value = expected_result
 
     result = by_pubmed_ids(cursor, pmids)
 
     assert result == expected_result
     for pmid in pmids:
         assert pmid in cursor.execute.call_args[0][1]["pubmed_ids"]
+
+
+@pytest.mark.parametrize(("pmids", "expected_result"), params)
+async def test_async_get_publications_by_pubmed_ids(
+    pmids, expected_result, async_cursor
+):
+    """Test getting multiple publications by pubmed ids."""
+    # Prepare the mock cursor
+    async_cursor.fetchall.return_value = expected_result
+
+    result = await async_by_pubmed_ids(async_cursor, pmids)
+
+    assert result == expected_result
+    for pmid in pmids:
+        assert pmid in async_cursor.execute.call_args[0][1]["pubmed_ids"]
