749b8a4fbd2f6e6353a443dc38204e010c2cdca5
Alexander Berger <alexander.berger@jax.org>
2024-04-19 09:20:31 -0400

    Adding thresholding db functions

---
+++ src/geneweaver/db/aio/threshold.py
@@ -0,0 +1,22 @@
+"""Geneset thresholding database functions."""
+
+from geneweaver.core.schema.score import GenesetScoreType
+from geneweaver.db.query import threshold as threshold_query
+from psycopg import AsyncCursor
+
+
+async def update_threshold(
+    cursor: AsyncCursor,
+    geneset_id: int,
+    geneset_score_type: GenesetScoreType,
+) -> None:
+    """Update the threshold of a geneset and its values.
+
+    :param cursor: An async database cursor.
+    :param geneset_id: The ID of the geneset to update.
+    :param geneset_score_type: The score threshold to set.
+    :return: Nothing.
+    """
+    await cursor.execute(
+        *threshold_query.set_geneset_threshold(geneset_id, geneset_score_type)
+    )
---
+++ src/geneweaver/db/query/threshold.py
@@ -0,0 +1,57 @@
+"""Query functions related to thresholding and score type."""
+from typing import Tuple
+
+from geneweaver.core.schema.score import GenesetScoreType
+from psycopg.sql import SQL, Composed
+
+
+def set_geneset_threshold(
+    geneset_id: int,
+    geneset_score_type: GenesetScoreType,
+) -> Tuple[Composed, dict]:
+    """Update the threshold of a geneset and its values.
+
+    :param geneset_id: The ID of the geneset to update.
+    :param geneset_score_type: An instance of GenesetScoreType, representing the new
+    threshold to set.
+    :return:  A query (and params) that can be executed on a cursor.
+    """
+    params = {
+        "geneset_id": geneset_id,
+        "score_type": int(geneset_score_type.score_type),
+        "threshold": geneset_score_type.threshold_as_db_string(),
+    }
+    query = SQL(
+        """
+        UPDATE geneset
+        SET gs_threshold_type = %(score_type)s,
+            gs_threshold = %(threshold)s
+        WHERE id = %(geneset_id)s;
+        UPDATE geneset_value
+            SET gsv_in_threshold = CASE
+        """
+    )
+    if geneset_score_type.threshold_low is not None:
+        query = query + SQL(
+            """
+            WHEN gsv_value BETWEEN %(threshold_low)s AND %(threshold)s THEN TRUE
+            """
+        )
+        params["threshold_low"] = geneset_score_type.threshold_low
+    else:
+        query = query + SQL(
+            """
+            WHEN gsv_value < %(threshold)s THEN TRUE
+            """
+        )
+    query = query + SQL(
+        """
+                ELSE FALSE
+            END
+        WHERE gs_id = %(geneset_id)s;
+        """
+    )
+
+    query = query.join(" ")
+
+    return query, params
