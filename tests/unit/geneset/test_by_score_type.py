cf0961f424594318fb1acbc827529f35da9376f6
Alexander Berger <alexander.berger@jax.org>
2024-05-15 16:25:19 -0400

    Update get geneset by score_type unit tests

--- tests/unit/geneset/test_by_score_type.py
+++ tests/unit/geneset/test_by_score_type.py
@@ -2,23 +2,17 @@
 
 import pytest
 from geneweaver.core.enum import ScoreType
-from geneweaver.core.schema.score import GenesetScoreType
 from geneweaver.db.aio.geneset import get as async_get
 from geneweaver.db.geneset import get
 
-gs_score_type = GenesetScoreType(score_type=ScoreType("p-value"), threshold=0.5)
-gs_score_type_w_low_threshold = GenesetScoreType(
-    score_type=ScoreType("p-value"), threshold=0.5, threshold_low=0.2
-)
-
 
 @pytest.mark.parametrize(
-    "score_type_params", [gs_score_type, gs_score_type_w_low_threshold]
+    "score_type", [score_type for score_type in ScoreType]
 )
-def test_by_score_type(score_type_params, example_genesets, cursor):
+def test_by_score_type(score_type, example_genesets, cursor):
     """Test the geneset.get function by score type using a mock cursor."""
     cursor.fetchall.return_value = example_genesets
-    result = get(cursor, geneset_score_type=score_type_params)
+    result = get(cursor, score_type=score_type)
     assert result == example_genesets
     assert cursor.execute.call_count == 1
     assert cursor.fetchone.call_count == 0
@@ -26,12 +20,12 @@ def test_by_score_type(score_type_params, example_genesets, cursor):
 
 
 @pytest.mark.parametrize(
-    "score_type_params", [gs_score_type, gs_score_type_w_low_threshold]
+    "score_type", [score_type for score_type in ScoreType]
 )
-async def test_async_by_score_type(score_type_params, example_genesets, async_cursor):
+async def test_async_by_score_type(score_type, example_genesets, async_cursor):
     """Test the geneset.get function by score type using a mock asynch cursor."""
     async_cursor.fetchall.return_value = example_genesets
-    result = await async_get(async_cursor, geneset_score_type=score_type_params)
+    result = await async_get(async_cursor, score_type=score_type)
     assert result == example_genesets
     assert async_cursor.execute.call_count == 1
     assert async_cursor.fetchone.call_count == 0
