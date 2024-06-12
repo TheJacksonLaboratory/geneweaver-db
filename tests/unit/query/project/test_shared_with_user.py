9a9cd928947865412c8112b4ad752a44b69e6cc4
francastell <fsophisco@yahoo.com>
2024-06-12 16:41:16 -0400

    G3-257 doc updates

--- tests/unit/query/project/test_shared_with_user.py
+++ tests/unit/query/project/test_shared_with_user.py
@@ -8,7 +8,7 @@ from geneweaver.db.query.project import PROJECT_FIELD_MAP, shared_with_user
 @pytest.mark.parametrize("limit", [None, 1, 10])
 @pytest.mark.parametrize("offset", [None, 1, 10])
 def test_all_kwargs(usr_id, limit, offset):
-    """Test all the kwarg combinations for query.gene.get."""
+    """Test all the kwarg combinations for query.project.shared_with_user."""
     query, params = shared_with_user(
         user_id=usr_id,
         limit=limit,
