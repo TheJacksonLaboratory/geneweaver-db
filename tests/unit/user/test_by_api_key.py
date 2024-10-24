56df5f005b97ca8940dc9f9746faf7d5513e606e
Alexander Berger <alexander.berger@jax.org>
2024-10-24 10:34:57 -0400

    Fixing user tests

--- tests/unit/user/test_by_api_key.py
+++ tests/unit/user/test_by_api_key.py
@@ -1,6 +1,6 @@
 """Test for the user.by_api_key function."""
 
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import pytest
 from geneweaver.db.user import by_api_key
@@ -8,14 +8,15 @@ from geneweaver.db.user import by_api_key
 from .const import MOCK_USER_DATA
 
 
+@patch("geneweaver.db.user.User", dict)
 @pytest.mark.parametrize(
     ("api_key", "expected_result"),
-    [(item["apikey"], [item]) for item in MOCK_USER_DATA],
+    [(item["apikey"], item) for item in MOCK_USER_DATA],
 )
 def test_by_api_key(api_key, expected_result):
     """Test the user by api key function with a mock cursor."""
     cursor = Mock()
-    cursor.fetchall.return_value = expected_result
+    cursor.fetchone.return_value = expected_result
     result = by_api_key(cursor, api_key)
     assert result == expected_result
     assert cursor.execute.call_count == 1
@@ -29,15 +30,15 @@ def test_by_api_key_execute_raises_error(all_psycopg_errors):
         by_api_key(cursor, "1")
 
     assert cursor.execute.call_count == 1
-    assert cursor.fetchall.call_count == 0
+    assert cursor.fetchone.call_count == 0
 
 
-def test_by_api_key_fetchall_raises_error(all_psycopg_errors):
-    """Test that the function raises an error when cursor.fetchall raises an error."""
+def test_by_api_key_fetchone_raises_error(all_psycopg_errors):
+    """Test that the function raises an error when cursor.fetchone raises an error."""
     cursor = Mock()
-    cursor.fetchall.side_effect = all_psycopg_errors("Error message")
+    cursor.fetchone.side_effect = all_psycopg_errors("Error message")
     with pytest.raises(all_psycopg_errors, match="Error message"):
         by_api_key(cursor, "1")
 
     assert cursor.execute.call_count == 1
-    assert cursor.fetchall.call_count == 1
+    assert cursor.fetchone.call_count == 1
--- tests/unit/user/test_by_sso_id.py
+++ tests/unit/user/test_by_sso_id.py
@@ -1,6 +1,6 @@
 """Test for the user.by_sso_id function."""
 
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import pytest
 from geneweaver.db.user import by_sso_id
@@ -8,14 +8,15 @@ from geneweaver.db.user import by_sso_id
 from .const import MOCK_USER_DATA
 
 
+@patch("geneweaver.db.user.User", dict)
 @pytest.mark.parametrize(
     ("sso_id", "expected_result"),
-    [(item["usr_sso_id"], [item]) for item in MOCK_USER_DATA],
+    [(item["usr_sso_id"], item) for item in MOCK_USER_DATA],
 )
 def test_by_api_key(sso_id, expected_result):
     """Test the user by sso id function with a mock cursor."""
     cursor = Mock()
-    cursor.fetchall.return_value = expected_result
+    cursor.fetchone.return_value = expected_result
     result = by_sso_id(cursor, sso_id)
     assert result == expected_result
     assert cursor.execute.call_count == 1
@@ -29,15 +30,15 @@ def test_by_sso_id_execute_raises_error(all_psycopg_errors):
         by_sso_id(cursor, "sso_id")
 
     assert cursor.execute.call_count == 1
-    assert cursor.fetchall.call_count == 0
+    assert cursor.fetchone.call_count == 0
 
 
-def test_by_sso_id_fetchall_raises_error(all_psycopg_errors):
-    """Test that the function raises an error when cursor.fetchall raises an error."""
+def test_by_sso_id_fetchone_raises_error(all_psycopg_errors):
+    """Test that the function raises an error when cursor.fetchone raises an error."""
     cursor = Mock()
-    cursor.fetchall.side_effect = all_psycopg_errors("Error message")
+    cursor.fetchone.side_effect = all_psycopg_errors("Error message")
     with pytest.raises(all_psycopg_errors, match="Error message"):
         by_sso_id(cursor, "sso_id")
 
     assert cursor.execute.call_count == 1
-    assert cursor.fetchall.call_count == 1
+    assert cursor.fetchone.call_count == 1
